6de7922db3f8451468c0871c3de5d765b82e81a3
kajisha <kajisha@gmail.com>
2018-07-12 17:54:05 +0900

    KC_H -> CTL_T(KC_H)

--- keyboards/ergodox_ez/keymaps/kajisha/keymap.c
+++ keyboards/ergodox_ez/keymaps/kajisha/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BSPC   |A/Ctrl|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
+ * | BSPC   |A/Ctrl|   S  |   D  |   F  |   G  |------|           |------|H/Ctrl|   J  |   K  |   L  |; / L2| Enter  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Cmd | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_TRNS,        KC_6,     KC_7,           KC_8,   KC_9,   KC_0,             KC_MINS,
         KC_RBRC,        KC_Y,     KC_U,           KC_I,   KC_O,   KC_P,             KC_BSLS,
-                        KC_H,     KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
+                 CTL_T(KC_H),     KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
         MEH_T(KC_NO),   KC_N,     KC_M,           KC_COMM,KC_DOT, GUI_T(KC_SLSH),   KC_RSFT,
                             KC_UP, KC_RIGHT,KC_NO,KC_NO,          KC_FN1,
         KC_LALT,        CTL_T(KC_ESC),
