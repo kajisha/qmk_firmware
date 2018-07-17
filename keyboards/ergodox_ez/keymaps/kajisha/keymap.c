0fec15fffde68cd7cfd3cddb829f8b81e1305c1f
kajisha <kajisha@gmail.com>
2018-07-17 14:40:09 +0900

    Remove CTL_T(KC_H) and Add CTL_T(KC_N)
    H key uses cursor moving on vim....

--- keyboards/ergodox_ez/keymaps/kajisha/keymap.c
+++ keyboards/ergodox_ez/keymaps/kajisha/keymap.c
@@ -22,9 +22,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   [  |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BSPC   |A/Ctrl|   S  |   D  |   F  |   G  |------|           |------|H/Ctrl|   J  |   K  |   L  |; / L2| Enter  |
+ * | BSPC   |A/Ctrl|   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Cmd | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |N/Ctrl|   M  |   ,  |   .  |//Cmd | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1|  '"  |AltShf| Left | Down |                                       |  Up  | Right|      |      | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
@@ -40,19 +40,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,     KC_2,           KC_3,   KC_4,   KC_5,             KC_EQL,
-        KC_TAB,         KC_Q,     KC_W,           KC_E,   KC_R,   KC_T,             KC_LBRC,
-        KC_BSPC, CTL_T(KC_A),     KC_S,           KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,     KC_X,           KC_C,   KC_V,   KC_B,             ALL_T(KC_NO),
-        LT(SYMB,KC_GRV),KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,KC_DOWN,
+        KC_GRV,              KC_1,     KC_2,           KC_3,   KC_4,   KC_5,             KC_EQL,
+        KC_TAB,              KC_Q,     KC_W,           KC_E,   KC_R,   KC_T,             KC_LBRC,
+        KC_BSPC,      CTL_T(KC_A),     KC_S,           KC_D,   KC_F,   KC_G,
+        KC_LSFT,             KC_Z,     KC_X,           KC_C,   KC_V,   KC_B,             ALL_T(KC_NO),
+        LT(SYMB,KC_GRV),  KC_QUOT,  LALT(KC_LSFT),  KC_LEFT,KC_DOWN,
                                                ALT_T(KC_APP), KC_LGUI,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-        KC_TRNS,        KC_6,     KC_7,           KC_8,   KC_9,   KC_0,             KC_MINS,
-        KC_RBRC,        KC_Y,     KC_U,           KC_I,   KC_O,   KC_P,             KC_BSLS,
-                 CTL_T(KC_H),     KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
-        MEH_T(KC_NO),   KC_N,     KC_M,           KC_COMM,KC_DOT, GUI_T(KC_SLSH),   KC_RSFT,
+        KC_TRNS,             KC_6,     KC_7,           KC_8,   KC_9,   KC_0,             KC_MINS,
+        KC_RBRC,             KC_Y,     KC_U,           KC_I,   KC_O,   KC_P,             KC_BSLS,
+                             KC_H,     KC_J,           KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_ENT,
+        MEH_T(KC_NO), CTL_T(KC_N),     KC_M,           KC_COMM,KC_DOT, GUI_T(KC_SLSH),   KC_RSFT,
                             KC_UP, KC_RIGHT,KC_NO,KC_NO,          KC_FN1,
         KC_LALT,        CTL_T(KC_ESC),
         KC_UP,
